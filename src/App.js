7dd2d8186c8a97013457a04cb5f82a288c1df6b8
mansurissa <mansurissa6@gmail.com>
2020-08-04 18:21:02 +0200

    bug fix deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Nav from "./Nav";
-import Home from "./home";
+import Home from "./Home";
 import About from "./about";
 import Shop from "./shop";
 import Single from "./single";
